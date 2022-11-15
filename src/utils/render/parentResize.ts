964968f8b176232fbef661820119db6e7b82350d
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2022-11-15 14:28:54 +0000

    when percentage max width is set and the parent element's width is resized but doesn't affect the table width, do not bother re-rendering it

--- src/utils/render/parentResize.ts
+++ src/utils/render/parentResize.ts
@@ -4,12 +4,15 @@ import {Render} from './render';
 // CAUTION-2
 export class ParentResize {
   private static shouldRerenderTable(etc: EditableTableComponent) {
+    const parentElement = etc.parentElement as HTMLElement;
     return (
       etc.tableDimensionsInternal.isPercentage &&
       // resize callback gets triggered on multiple occassions when the parent width has not changed:
       // on startup, after table has been resized, when parent height is changed and when column height is changed
       // this condition prevents the table from re-rendering itself when the above occurs
-      etc.tableDimensionsInternal.recordedParentWidth !== (etc.parentElement as HTMLElement).offsetWidth
+      etc.tableDimensionsInternal.recordedParentWidth !== parentElement.offsetWidth &&
+      // if the parent is resized to a width that does not impact the table width, do not bother re-rendering it
+      (etc.tableDimensionsInternal.maxWidth === undefined || etc.offsetWidth > parentElement.offsetWidth)
     );
   }
 
