ea79c695f7589243a48bccfd9dcf1fc0e776d806
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2023-03-14 18:23:55 +0000

    added programmatic update example

--- website/src/components/table/tableContainerProgrammaticUpdates.js
+++ website/src/components/table/tableContainerProgrammaticUpdates.js
@@ -1,7 +1,27 @@
 import TableContainer, {extractChildTableElement} from '@site/src/components/table/tableContainer';
 import React from 'react';
 
-function updateCell(tableElement) {
+function updateStockCell(tableElement) {
+  if (!tableElement?.isConnected) return;
+  setTimeout(() => {
+    const rowIndex = Math.floor(Math.random() * 5 + 1);
+    const columnIndex = Math.floor(Math.random() * 5 + 1);
+    let newText = '';
+    if (columnIndex === 1) {
+      newText = `$${Math.round(Math.random() * 1000 * 10) / 100}`;
+    } else if (columnIndex === 2) {
+      newText = `$${Math.round(Math.random() * 1000 * 10) / 100}`;
+    } else if (columnIndex === 3) {
+      newText = `${((Math.round(Math.random()) ? 1 : -1) * Math.round(Math.random() * 1.5 * 10)) / 10}%`;
+    } else {
+      newText = `${Math.round(Math.random() * 2.5 * 10) / 10}%`;
+    }
+    tableElement.updateCell({newText, rowIndex, columnIndex});
+    updateStockCell(tableElement);
+  }, 10);
+}
+
+function updateComputerCell(tableElement) {
   if (!tableElement?.isConnected) return;
   setTimeout(() => {
     const rowIndex = Math.floor(Math.random() * 5 + 1);
@@ -17,21 +37,22 @@ function updateCell(tableElement) {
       newText = `${Math.round(Math.random() * 1.5 * 10) / 10}Mbps`;
     }
     tableElement?.updateCell({newText, rowIndex, columnIndex});
-    updateCell(tableElement);
+    updateComputerCell(tableElement);
   }, 40);
 }
 
-export default function TableContainerProgrammaticUpdates({children}) {
+export default function TableContainerProgrammaticUpdates({children, isStock, minHeight}) {
   const programmaticUpdateTableContainer = React.useRef(null);
   if (programmaticUpdateTableContainer.current) {
+    const updateFunc = isStock ? updateStockCell : updateComputerCell;
     setTimeout(() => {
       const activeTableReference = extractChildTableElement(programmaticUpdateTableContainer.current?.children[0]);
-      updateCell(activeTableReference);
+      updateFunc(activeTableReference);
     });
   }
   return (
     <div ref={programmaticUpdateTableContainer}>
-      <TableContainer>{children}</TableContainer>
+      <TableContainer minHeight={minHeight}>{children}</TableContainer>
     </div>
   );
 }
