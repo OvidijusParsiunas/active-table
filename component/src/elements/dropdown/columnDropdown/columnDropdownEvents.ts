206fa69f75caeb21eee426e18dbb5f5e238fdfc7
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2023-04-11 21:02:46 +0100

    0.0.230 when tab is clicked on an open column dropdown - the next column's dropdown is opened

--- component/src/elements/dropdown/columnDropdown/columnDropdownEvents.ts
+++ component/src/elements/dropdown/columnDropdown/columnDropdownEvents.ts
@@ -5,6 +5,19 @@ import {ColumnDropdown} from './columnDropdown';
 import {DropdownItem} from '../dropdownItem';
 
 export class ColumnDropdownEvents {
+  private static focusNextColumnDropdown(at: ActiveTable, event: KeyboardEvent) {
+    event.preventDefault();
+    ColumnDropdown.processTextAndHide(at);
+    let columnIndex = at._focusedElements.cell.columnIndex as number;
+    if (columnIndex === at._columnsDetails.length - 1) columnIndex = -1;
+    const columnDetails = at._columnsDetails[columnIndex + 1];
+    if (at._defaultColumnsSettings.columnDropdown?.displaySettings?.openMethod?.cellClick) {
+      columnDetails.elements[0].click();
+    } else {
+      columnDetails.columnDropdownCellOverlay.click();
+    }
+  }
+
   public static onKeyDown(this: ActiveTable, dropdownElement: HTMLElement, event: KeyboardEvent) {
     if (event.key === KEYBOARD_KEY.ENTER) {
       const itemElement = event.target as HTMLElement;
@@ -16,8 +29,11 @@ export class ColumnDropdownEvents {
       }
     } else if (event.key === KEYBOARD_KEY.ESCAPE) {
       ColumnDropdown.processTextAndHide(this);
+    } else if (event.key === KEYBOARD_KEY.TAB && this._columnsDetails.length > 0) {
+      ColumnDropdownEvents.focusNextColumnDropdown(this, event);
+    } else {
+      DropdownEvents.itemKeyNavigation(this.shadowRoot as ShadowRoot, dropdownElement, event);
     }
-    DropdownEvents.itemKeyNavigation(this.shadowRoot as ShadowRoot, dropdownElement, event);
   }
 
   public static set(at: ActiveTable, dropdownElement: HTMLElement) {
