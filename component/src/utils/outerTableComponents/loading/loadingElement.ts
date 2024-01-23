c89ff7e0080eb284d1c8b5feb7b6afe1919ef4c2
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2024-01-23 22:51:39 +0000

    the loading container now uses the tableStyle by default

--- component/src/utils/outerTableComponents/loading/loadingElement.ts
+++ component/src/utils/outerTableComponents/loading/loadingElement.ts
@@ -1,4 +1,6 @@
 import {LoadingStyles} from '../../../types/loadingStyles';
+import {GenericObject} from '../../../types/genericObject';
+import {TableStyle} from '../../../types/tableStyle';
 import {CSSStyle} from '../../../types/cssStyle';
 
 export class LoadingElement {
@@ -9,12 +11,31 @@ export class LoadingElement {
     return spinnerElement;
   }
 
-  public static create(loadingStyle?: LoadingStyles) {
-    const loadingElement = document.createElement('div');
-    loadingElement.className = 'loading-container';
-    Object.assign(loadingElement.style, loadingStyle?.container);
+  private static applyTableStyles(styles: (keyof CSSStyleDeclaration)[], tStyle: TableStyle, loadingElement: HTMLElement) {
+    styles.forEach((style) => {
+      if (tStyle[style]) {
+        (loadingElement.style as unknown as GenericObject)[style as string] = tStyle[style] as string;
+      }
+    });
+  }
+
+  // prettier-ignore
+  private static createContainer(loadingStyle?: LoadingStyles, tableStyle?: TableStyle) {
+    const containerElement = document.createElement('div');
+    containerElement.className = 'loading-container';
+    if (tableStyle) {
+      LoadingElement.applyTableStyles(
+        ['width', 'minWidth', 'maxHeight', 'height', 'minHeight', 'maxHeight', 'border', 'borderColor', 'borderWidth'],
+        tableStyle, containerElement);
+    }
+    Object.assign(containerElement.style, loadingStyle?.container);
+    return containerElement;
+  }
+
+  public static create(loadingStyle?: LoadingStyles, tableStyle?: TableStyle) {
+    const containerElement = LoadingElement.createContainer(loadingStyle, tableStyle);
     const spinnerElement = LoadingElement.createSpinner(loadingStyle?.spinner);
-    loadingElement.appendChild(spinnerElement);
-    return loadingElement;
+    containerElement.appendChild(spinnerElement);
+    return containerElement;
   }
 }
