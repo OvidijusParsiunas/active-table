79be45284ccaf1ebf5984e463368925a9a91b408
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2024-02-20 20:15:14 +0000

    updating the CSV paste logic to not add extra rows if displayAddNewRow is set to false

--- component/src/utils/paste/CSV/overwriteCellsViaCSVOnPaste.ts
+++ component/src/utils/paste/CSV/overwriteCellsViaCSVOnPaste.ts
@@ -2,9 +2,17 @@ import {FocusedCellUtils} from '../../focusedElements/focusedCellUtils';
 import {InsertMatrix} from '../../programmaticUpdates/insertMatrix';
 import {ParseCSVClipboardText} from './parseCSVClipboardText';
 import {FocusedCell} from '../../../types/focusedCell';
+import {TableData} from '../../../types/tableData';
 import {ActiveTable} from '../../../activeTable';
 
 export class OverwriteCellsViaCSVOnPaste {
+  private static trimCSVRowsIfCantCreateNew(CSV: TableData, data: TableData, rowIndex: number) {
+    if (data.length < CSV.length + rowIndex) {
+      return CSV.slice(0, CSV.length - (CSV.length + rowIndex - data.length));
+    }
+    return CSV;
+  }
+
   private static focusOriginalCellAfterProcess(at: ActiveTable, process: () => void) {
     const {element, rowIndex, columnIndex} = at._focusedElements.cell as Required<FocusedCell>;
     process();
@@ -15,7 +23,8 @@ export class OverwriteCellsViaCSVOnPaste {
   public static overwrite(at: ActiveTable,
       clipboardText: string, event: ClipboardEvent, rowIndex: number, columnIndex: number) {
     event.preventDefault();
-    const CSV = ParseCSVClipboardText.parse(clipboardText);
+    let CSV = ParseCSVClipboardText.parse(clipboardText);
+    if (!at.displayAddNewRow) CSV = OverwriteCellsViaCSVOnPaste.trimCSVRowsIfCantCreateNew(CSV, at.data, rowIndex);
     OverwriteCellsViaCSVOnPaste.focusOriginalCellAfterProcess(at,
       InsertMatrix.insert.bind(this, at, CSV, rowIndex, columnIndex));
   }
