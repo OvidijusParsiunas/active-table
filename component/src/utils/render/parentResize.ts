6a53932a72a4e3518ae709e29144b270446e8be2
Irshad Ahamed M B <irshadmb@gmail.com>
2024-09-07 10:27:34 +0530

    Update parentResize.ts
    Issue fix for #16

--- component/src/utils/render/parentResize.ts
+++ component/src/utils/render/parentResize.ts
@@ -13,18 +13,23 @@ export class ParentResize {
   }
 
   private static shouldRerenderTable(at: ActiveTable) {
-    const parentElement = at.parentElement as HTMLElement;
-    if (ParentResize.doesOverflowNeedRerender(at, parentElement)) return true;
-    return (
-      at._tableDimensions.isPercentage &&
-      // resize callback gets triggered on multiple occassions when the parent width has not changed:
-      // on startup, after table has been resized, when parent height is changed and when column height is changed
-      // this condition prevents the table from re-rendering itself when the above occurs
-      at._tableDimensions.recordedParentWidth !== parentElement.offsetWidth &&
-      // if the parent is resized to a width that does not impact the table width, do not bother re-rendering it
-      (at._tableDimensions.maxWidth === undefined || at.offsetWidth > parentElement.offsetWidth)
-    );
-  }
+  if (!at) return false; // Ensure 'at' is not null
+  const parentElement = at.parentElement as HTMLElement;
+  if (!parentElement) return false; // Ensure 'parentElement' is not null
+
+  if (ParentResize.doesOverflowNeedRerender(at, parentElement)) return true;
+
+  return (
+    at._tableDimensions.isPercentage &&
+    // Resize callback gets triggered on multiple occasions when the parent width has not changed:
+    // on startup, after table has been resized, when parent height is changed and when column height is changed
+    // This condition prevents the table from re-rendering itself when the above occurs
+    at._tableDimensions.recordedParentWidth !== parentElement.offsetWidth &&
+    // If the parent is resized to a width that does not impact the table width, do not bother re-rendering it
+    (at._tableDimensions.maxWidth === undefined || at.offsetWidth > parentElement.offsetWidth)
+  );
+}
+
 
   public static resizeCallback() {
     const at = this as unknown as ActiveTable;
